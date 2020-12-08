a3897d6c855f425567c194b4b412c1c1fdb19e1a
Ethan Friedman <32680186+etfriedman@users.noreply.github.com>
2020-12-08 11:49:34 -0500

    Update emails.py

--- emails.py
+++ emails.py
@@ -2,14 +2,11 @@ import csv
 import random
 from more_itertools import flatten
 from pprint import pprint
-
 names = {"Grade 9":[],"Grade 10":[],"Grade 11":[],"Grade 12":[]}
-
 with open("students.csv") as f:
     file = csv.reader(f, delimiter=',')
     for row in file:
         names[row[0]].append(row[1])
-    
 
 for grade, name in names.items():
     for n in name:
@@ -21,7 +18,6 @@ def get_emails(num):
     for grade, emails in names.items():
         final_emails.append(random.sample(emails,k=num))
     final_emails = list(flatten((item) for item in final_emails))    
-    print(', '.join(final_emails))  
-
-
+    print(', '.join(final_emails)) 
+    
 get_emails(int(input("# of emails from each grade\n>")))
